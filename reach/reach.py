117350a62cb7a6f4acf59c186287a65fe18ee7a2
ljf20251127 <3413134712@qq.com>
2026-07-27 12:42:39 +0800

    pink mujoco 末端位置参考系对齐

--- reach/reach.py
+++ reach/reach.py
@@ -1,11 +1,11 @@
 import mujoco
 import numpy as np
 
-model = mujoco.MjModel.from_xml_path("franka_emika_panda/scene.xml")
+mj_model = mujoco.MjModel.from_xml_path("robot_models/franka_emika_panda/scene.xml")
 
 # 从 model 中提取关节限位（自动匹配）
-q_min = np.array([model.jnt_range[i][0] for i in range(model.njnt)])
-q_max = np.array([model.jnt_range[i][1] for i in range(model.njnt)])
+q_min = np.array([mj_model.jnt_range[i][0] for i in range(mj_model.njnt)])
+q_max = np.array([mj_model.jnt_range[i][1] for i in range(mj_model.njnt)])
 
 q_min = q_min[:-2]
 q_max = q_max[:-2]
@@ -18,14 +18,43 @@ q_random = np.random.uniform(q_min, q_max)
 print("q_random:", q_random)
 
 # 没有site
-for i in range(model.nsite):
-    print(i, model.site(i).name)
+for i in range(mj_model.nsite):
+    print(i, mj_model.site(i).name)
 
-print("model.nsite:", model.nsite)
+print("mj_model.nsite:", mj_model.nsite)
 
 # 查看所有 body 名称
-for i in range(model.nbody):
-    print(f"Body {i}: {mujoco.mj_id2name(model, mujoco.mjtObj.mjOBJ_BODY, i)}")
+# for i in range(mj_model.nbody):
+#    print(f"Body {i}: {mujoco.mj_id2name(mj_model, mujoco.mjtObj.mjOBJ_BODY, i)}")
 
+mj_data = mujoco.MjData(
+    mj_model
+)
 
+site_id = mujoco.mj_name2id(
+    mj_model,
+    mujoco.mjtObj.mjOBJ_SITE,
+    "panda_hand_tcp"
+)
+q = np.array([
+    0.0,
+    -0.785398,
+    0.0,
+    -2.35619,
+    0.0,
+    1.5708,
+    0.785398,
+    0,
+    0,
+])
+
+mj_data.qpos[:9] = q
+
+mujoco.mj_forward(
+    mj_model,
+    mj_data
+)
+
+mujoco_pos = mj_data.site_xpos[site_id]
     
+print("MuJoCo pos:", mujoco_pos)
